da7a11b289711c52ff17186a4823c449607eba71
Mohinabonu <apple@MacBook-Air.local>
2018-05-08 00:35:39 +0500

    ready to test

--- container.py
+++ container.py
@@ -1,45 +1,65 @@
+import math
+from random import randint
+minLength = 10
+maxLength = 100
 class Container:   # angle is not taken into consideration beacause it will not affect to the volume
+	isFilled = False
 	def __init__(self, volume):
 		self.volume   = volume   # volume in m3
-		self.isFilled = False
 
-	def getInput(self, state):
-		state = 'Insert {} of {} separated by spaces: '.format(state, self.name)
-		arguments     = input(state)
-		argumentArray = arguments.strip().split(' ')
-		try:
-			return (int(argumentArray[0]), int(argumentArray[1]))
-		except:
-			print("Enter 2 or more numbers, please. Try again")
-			self.getInput(state)
+	def getInput(self, param):
+		while True:
+			arguments = input(self.name+' - '+param+': ').strip()
+			if len(arguments)==0:
+				arg1 = randint(minLength, maxLength)
+				arg2 = randint(minLength, maxLength)
+				arg3 = None
+				break
+			else:
+				try:
+					argumentArray = [int(x) for x in arguments.split(' ')]
+					arg1 = argumentArray[0]
+					arg2 = argumentArray[1]
+				except IndexError:
+					print("Enter 2 numbers, at least, please. Try again")
+					continue
+				else:
+					arg3 = argumentArray[2] if len(argumentArray)>2 else None
+					break
+		return (arg1, arg2, arg3)
 
-	def fill(liquid):
+
+	def fill(self, liquid):
+		self.filledWith = liquid
 		self.mass     = liquid.density * self.volume
 		self.isFilled = True
 
 class SquareContainer(Container):
 	name = 'Square prism'
-	def __init__(self, edge=None, height=None): # I created an option to give parameters as an argument even if the task does not require it
+	def __init__(self, edge=None, height=None, angle=90): # I created an option to give parameters as an argument even if the task does not require it
 		if edge is None or height is None:
-			edge, height = self.getInput('edge, height and angle')
+			edge, height, angle = self.getInput('edge, height and angle (defaults to 90)')
 		self.edge   = edge   # cm
 		self.height = height # cm
-		self.volume = edge * edge * height
+		self.angle  = angle or 90 # degrees
+		self.volume = int(edge * edge * height)
 
 class CircleContainer(Container):
 	name = 'Cylinder'
-	def __init__(self, radius=None, height=None): # I created an option to give parameters as an argument even if the task does not require it
+	def __init__(self, radius=None, height=None, angle=90): # I created an option to give parameters as an argument even if the task does not require it
 		if radius is None or height is None:
-			radius, height = self.getInput('radius, height and angle')
+			radius, height, angle = self.getInput('radius, height and angle')
 		self.radius = radius  # cm
 		self.height = height  # cm
-		self.volume = math.pi * radius * radius * height
+		self.angle  = angle or 90 # degrees
+		self.volume = int(math.pi * radius * radius * height)
 
 class TriangleContainer(Container):   # triangle with equal edges
 	name = 'Triangular prism'
-	def __init__(self, edge=None, height=None): # I created an option to give parameters as an argument even if the task does not require it
+	def __init__(self, edge=None, height=None, angle=90): # I created an option to give parameters as an argument even if the task does not require it
 		if edge is None or height is None:
-			edge, height = self.getInput('edge, height and angle')
+			edge, height, angle = self.getInput('edge, height and angle')
 		self.edge   = edge   # cm
 		self.height = height # cm
-		self.volume = math.sqrt(3)/4 * edge * edge
+		self.angle  = angle or 90 # degrees
+		self.volume = int(math.sqrt(3)/4 * edge * edge)
--- run.py
+++ run.py
@@ -3,32 +3,69 @@ import container
 import random
 
 class randomLiquidsOnContainers:
+
 	liquidTypes  = [liquid.Alcohol(), liquid.Petrol(), liquid.Water()]
-	class Containers: 
-		types = [container.SquareContainer, container.CircleContainer, container.TriangleContainer]
+	containerTypes = [container.SquareContainer, container.CircleContainer, container.TriangleContainer]
+	containers = []
+
+	def chooseLiquid(self):
+		count = len(self.liquidTypes)
+		import random
+		r = random.randint(0, count-1)
+		return self.liquidTypes[r]
+
+	def chooseContainers(self):
 		counts    = []
-		containers= []
-		def addCount(self, count):
-			self.counts += [int(count.strip())] # adds to the array
-		def getTotal(self):
-			return sum(self.counts)
-		def __init__(self):
-			state = "How many {}s do you want to have?\n"
-			for containerType in self.types:
-				while True:
-					try:
-						self.addCount(input(state.format(containerType.name)))
-					except:
-						print("Enter a number, please.")
-						continue
-					break
+		state = "How many %ss do you want to have?   "
+
+		for containerType in self.containerTypes:
+			while True:
+				try:
+					countIn = input(state % containerType.name).strip()
+					counts += [int(countIn)]
+				except:
+					print("Enter a number, please.")
+					continue
+				break
+		return counts
 
 	def __init__(self):
-		cont = self.Containers()
-		if cont.getTotal()<5:
+		counts = self.chooseContainers()
+		if sum(counts)<5:
 			print("Total is less than 5. Enter again, please")
 			self.__init__()
-		for index, containerType in enumerate(cont.types):
-			cont.containers += [containerType] * counts[index]
+		print("Insert parameters of containers separated by spaces (leave empty for random generation): ")
+		for index, containerType in enumerate(self.containerTypes):
+			for i in range(counts[index]):
+				self.containers += [containerType()]
+
+	def fillWithLiquid(self):
+		for container in self.containers:
+			randomLiquid = self.chooseLiquid()
+			container.fill(randomLiquid)
+
+	def printTable(self):
+		titles = ['Container Type', 'h', 'a', 'r', 'd', 'V', 'liquid', 'p', 'm']
+		name   = [x.name for x in self.containers]
+		h = [x.height for x in self.containers]
+		a = [getattr(x, 'edge', '-') for x in self.containers]
+		r = [getattr(x, 'radius', '-') or '-' for x in self.containers]
+		d = [x.angle for x in self.containers]
+		V = [x.volume for x in self.containers]
+		liquid = [x.filledWith for x in self.containers]
+		liquidName = [x.filledWith.__class__.__name__ for x in self.containers]
+		p = [x.density for x in liquid]
+		m   = [x.mass for x in self.containers]
+
+		data   = [titles] + list(zip(name, h, a, r, d, V, liquidName, p, m))
+
+		for i, d in enumerate(data):
+			line = '|'.join(str(x).ljust(16) for x in d)
+			print(line)
+			if i == 0:
+				print('-' * len(line))
+
 
 a = randomLiquidsOnContainers()
+a.fillWithLiquid()
+a.printTable()
